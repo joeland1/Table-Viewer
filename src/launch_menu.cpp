5e4205ca83b507100b27fa3a87e392b802371408
joeland1 <55666119+joeland1@users.noreply.github.com>
2021-01-21 08:56:27 -0500

    quick work

--- src/launch_menu.cpp
+++ src/launch_menu.cpp
@@ -11,7 +11,32 @@ Launch::Launch(QWidget *parent):QWidget(parent)
 {
   QHBoxLayout *layout = new QHBoxLayout();
 
-  QTreeWidgetItem *item = new QTreeWidgetItem();
+  //assuming we are using sqlite3 for now
+  QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE", "bot");
+  db.setForwardOnly(true);
+  db.setDatabaseName("name_for_now.db");
+
+  if(db.open())
+  {
+    query.exec("SELECT count(*) FROM sqlite_master WHERE TYPE='table';");
+    int number_of_tables =  query.next().value(0);
+
+    for(int i=0;i<number_of_tables;i++)
+    {
+      
+    }
+
+    //QSqlRecord rec = q.record();
+  }
+  else
+  {
+    QMessageBox::warning(this, tr("Cannot open database"), tr("Please try again."), QMessageBox::Close);
+  }
+
+
+
+
+  QTreeWidgetItem *header = new QTreeWidgetItem();
     item->setText(0,"ez");
   QTreeWidgetItem *sub = new QTreeWidgetItem();
     sub->setText(0,"sub");
